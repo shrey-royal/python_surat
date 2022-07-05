65f5d886686959f5e3af9ba275c7eea25559aa77
shrey k <shreykadia.royal@gmail.com>
2022-07-05 11:52:13 +0530

    print statement with arguments

---
+++ first.py
@@ -0,0 +1,72 @@
+# compiler -> all the code will be compiled then it will show errors
+# interpreter -> it will compile code line by line and if there is an error in the code then it will stop at that line
+
+
+# Python is an interpreted language
+
+'''
+print("Hello World!")
+print() # this is an empty print function
+print("new line") # this print fn have some data in it
+
+print("this sentence will be written in a new line")
+
+print("Write about yourself using the printf function")
+
+print("My NAme is ______")
+print("My Father name ______")
+
+print("jfkdsnflk djsg kjfdnodf gdkfnlfdng df")
+
+
+# single line comment
+
+'' '
+multi
+line 
+comment
+'' '
+
+"""
+multi 
+line comment
+"""
+
+
+"""
+> the comments are used for developers to understand what's in the code.
+
+> comments are used to explain the logic that is written in any program.
+
+> The comments are used by developers for the developers to understand the logic in the code or in a program.
+
+
+"""
+
+'''
+
+
+print("data \nthis \ndata will be printed on new line") # input -> string
+print("here is some data", end=". this will be the end of the string\n")
+
+
+print("string 1", "string 2", "string 3", sep=" | ")
+
+'''
+20 lines 
+using the arguments given below
+end=, sep=
+
+'''
+
+
+# print("another_data")
+
+
+# end= means at the end of the print function.
+# \n -> new line --> escape sequence
+# \t -> tab
+
+# default end= arguments have \n as value.
+
+# arguments -> arguments are inputs 
