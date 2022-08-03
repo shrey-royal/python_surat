1d4ccca7df17ccbad5dfd11c9db7a4151f03e49d
Shrey k <srkkadia@gmail.com>
2022-08-03 10:16:38 +0530

    String methods

---
+++ String/3Aug.py
@@ -0,0 +1,28 @@
+greetings = "Hello, Good Morning!"
+
+print(len(greetings))
+
+# print(greetings.encode(encoding="ascii", errors="xmlcharrefreplace"))
+
+# print(greetings.expandtabs(tabsize=50))
+# print(greetings.endswith("!"))
+# print(greetings.format(name = "Shrey"))
+# print(greetings.find("oo"))
+
+
+# name = {"name": "John", "age": "21"}  # dictionary
+# print("{name} {age}".format_map(name))
+
+# print(greetings.index("o"))
+
+# new_string = "12-4"
+# print(new_string.isalnum()) # this will return true if all the characters are alphanumeric
+# print(new_string.isalpha()) # this will return true if all the characters are alphabets
+# print(new_string.isdigit()) # this will return true if all the characters are digits
+
+
+
+# tup = ("John", "Marcos", "Stranger Things")   # tuple
+# print("-".join(tup))
+
+# Task -:  you have to try all the methos of string that starts with i
