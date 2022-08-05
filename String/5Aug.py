0751c11129818127a46b0d35a6aa86b46a0388d1
Shrey K <srkkadia@gmail.com>
2022-08-05 11:02:40 +0530

    string methods

---
+++ String/5Aug.py
@@ -0,0 +1,46 @@
+# my_string = "     - ---   - - Good Morning Everyone___________"
+# new_string = "___"
+# tup = ("1", "2", "3", "4")
+
+# print(my_string.join(tup))
+# _Good Morning Everyone_Good Mornign Everyone_
+
+
+# print(len(my_string))
+# print(my_string.ljust(40, "_"))
+# print(my_string.lower())
+# print(my_string.lstrip("- "))
+
+
+my_string = "jigar bhajiawala"
+# print(my_string.partition(" "))
+
+# it will return tuple -> it has 3 elements
+# left string, actual seperator, right string
+
+
+# print(my_string.removeprefix("J"))
+# print(my_string.removesuffix("wala"))
+# print(my_string.replace("Jigar", "Ritika"))
+# print(my_string.replace("a", "e"))
+# print(len(my_string)) # 15 -> 16
+# print(my_string.rfind("l"))
+# print(my_string.rjust(60, " "))
+# print(my_string.rindex("a"))
+# print(my_string.rpartition(" "))
+# print(my_string.rsplit(" ")) # it will return a list
+# left string, right string
+
+# print(my_string.rstrip("_").lstrip(" -"))
+
+# print(my_string.splitlines())
+# print(my_string.startswith("J"))
+# print(my_string.swapcase())
+# print(my_string.strip("- _"))
+# print(my_string.split(" "))
+
+# print(my_string.title())
+# print(my_string)
+# print(my_string.translate({97:98, 98:97}))
+
+# print(my_string.)
