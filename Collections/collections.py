66b4616692533e687c4596991a6906706810b0ee
Shrey K <srkkadia@gmail.com>
2022-08-09 11:26:27 +0530

    Collections - list

---
+++ Collections/collections.py
@@ -0,0 +1,30 @@
+# collections in python
+
+"""
+-> Collections in python are used to store data in a structured way.
+-> 4 types of collections in python:
+    1. List
+    2. Tuple
+    3. Set
+    4. Dictionary
+
+
+-> ordered, unordered, indexed, unindexed collections
+
+1. List - ordered, indexed collection
+    -> list is a mutable ordered collection of objects
+    -> duplicates are allowed
+
+2. Tuple - ordered, unindexed collection
+    -> tuple is a immutable ordered collection of objects
+    -> duplicates are allowed
+
+3. Set - unordered, unindexed collection
+    -> set is a mutable unordered collection of objects
+    -> unique elements, no duplicate elements
+
+4. Dictionary - unordered, indexed collection
+    -> dictionary is a mutable unordered collection of key-value pairs
+    -> keys must be unique, values can be duplicate
+
+"""
---
+++ Collections/list/intro.py
@@ -0,0 +1,28 @@
+myList = [] # empty list
+
+myList1 = [1, 2, 3, 4, 5] # list with 5 elements
+
+# print(myList)
+print(myList1)
+
+# print(myList1[0:3])
+# print(myList1[3:])
+
+myList1.append(int(input("Enter a number: "))) # append a number to the list
+
+
+for i in myList1:
+    print(i)
+
+print(myList1)
+
+"""
+Task -: 
+
+1. Print a list containing at least 20 numbers
+2. Scan a list and print the even numbers
+3. Scan a list and print the odd numbers
+4. Scan a list and print the numbers which are divisible by 3
+5. Ask user for a number and add that many numbers in the list and print the list
+
+"""
