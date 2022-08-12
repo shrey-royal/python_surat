b142c4dc56b9fdc6d52011e0a83042878c0d3f2c
Shrey K <srkkadia@gmail.com>
2022-08-12 09:55:55 +0530

    Access List Items

---
+++ Collections/list/12Aug.py
@@ -0,0 +1,67 @@
+# list -> ordered, indexed collection and it allows the duplicates elements
+
+# str = "Hello"
+
+# print(str[0])
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(list1[0:4])   # print the first 4 elements of the list (0 -> n-1)
+
+strlist = ["Hello", "World", "Python"]
+
+print(strlist)
+
+mixlist = [1, "Hello", 2, "World", 3, "Python", 1.2, "Surat", 2.3, "Gujarat", "Hello", "Hello"]
+
+print(mixlist)
+
+# list is class in python
+
+# how to identify the type of the list
+print(type(list1))
+# a list is always enclosed in square brackets -> names = []
+
+# how to access the elements of the list
+
+# to check the length of the list
+
+print(len(list1)) # print the length of the list
+# len method is used to prin the length of the list
+
+print(len(mixlist))
+
+# counter = 0
+# for i in mixlist: # loop through the list
+#     counter+=1
+
+# print(counter)
+
+
+# list2 = list(("apple", "cherry", "banana")) # empty list
+list2 = ["apple", "cherry", "banana"]
+
+# A constructor is a default method which is used to create an object of the class
+# a constructor is a special method that automatically invoked when an object is created
+
+print(list2)
+
+
+list23 = []
+
+
+'''
+# Object oriented programming - OOP
+
+# class - collection of (similar) objects
+# objects - a object is a real world entity
+
+# example of object are -> person, car, book, etc.
+
+object -> iphone 13, s22 ultra
+class -> phone
+
+class smartPhone -> nokia, samsung, huawei, vivo, oppo, XIOMI, etc.
+'''
+
+
