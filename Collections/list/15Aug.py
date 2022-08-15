59d24db935c6f478d9a014319b32e299655834c9
Shrey K <srkkadia@gmail.com>
2022-08-15 09:45:06 +0530

    Change items, List methods

---
+++ Collections/list/15Aug.py
@@ -0,0 +1,98 @@
+# add or change new items to the list
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'strawberry', 'raspberry', 'blueberry', 'orange']
+
+# print("\n\n",fruits) # \n means new line
+
+# change the first item in the list
+
+# print("Older first item in the list was ", fruits[0])
+# fruits[0] = "grape"
+# print("New first item in the list is ", fruits[0])
+# print(fruits)
+
+# add new item to the list
+
+# fruits.append("grape")
+
+# print(fruits)
+
+#list methods
+
+# fruits.append("plum") # add new item to the list
+# print(fruits)
+
+# fruits.clear() # clear the list
+
+# print(fruits)
+
+# mylist = fruits.copy() # copy the list
+
+# newlist = fruits # here it will give another label(name) to it, not a copy of it
+
+# print(newlist)
+
+# print("id of fruits: ", id(fruits))
+# print("id of newlist: ", id(mylist))
+
+# print(fruits.count("orange")) # count the number of items in the list
+
+# print(fruits.index("orange")) # find the index of the item in the list
+
+# fruits.extend(["grape", "plum"]) # extend the list with new items
+# print(fruits)
+
+# print(fruits)
+# fruits.insert(1, "grape") # insert new item to the list at the specified index
+# print(fruits)
+
+# fruits.remove("orange") # remove the item from the list
+# print(fruits)
+
+# fruits.reverse() # reverse the list
+# print(fruits)
+
+# fruits.pop() # remove the last item from the list
+# print(fruits)
+
+# fruits.sort() # sort the list
+# if reverse=True given, it will sort in reverse order
+# print(fruits)
+
+'''
+List Methods-:
+
+append()	Adds an element at the end of the list
+clear()		Removes all the elements from the list
+copy()		Returns a copy of the list
+count()		Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()		Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()		Removes the element at the specified position
+remove()	Removes the item with the specified value
+reverse()	Reverses the order of the list
+sort()		Sorts the list
+
+
+Tasks in list methods:
+
+1. Create a list of fruits
+2. Add new item to the list
+3. Change the first item in the list
+4. Add new item to the list
+5. Clear the list
+6. Copy the list
+7. Count the number of items in the list
+8. Find the index of the first item with the specified value
+9. Add the elements of a list (or any iterable), to the end of the current list
+10. Add an element at the specified position
+11. Remove the element at the specified position
+12. Removes the item with the specified value
+13. Reverse the order of the list
+14. Sort the list
+15. Remove the last item from the list
+
+Do all 15 tasks in the same program.
+
+'''
