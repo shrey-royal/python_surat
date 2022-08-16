6b3f193971cd089948ad0d01cb7b5a26b6effbdb
Shrey K <srkkadia@gmail.com>
2022-08-16 11:54:54 +0530

    Loop Lists and List Comprehension

---
+++ Collections/list/16Aug.py
@@ -0,0 +1,65 @@
+# join the lists
+
+# fruits = ['apple', 'banana', 'cherry']
+# vegetables = ['beetroot', 'carrot', 'potato']
+
+# fruits_and_vegetables = fruits + vegetables # concatenation of two lists
+
+# print(fruits_and_vegetables)
+
+# fruit = ["apple", "cherry"] * 5 # repeat the list
+
+# print(fruit)
+
+# numbers = [x for x in range(1, 100)]
+
+# print(len(numbers))
+
+# list comprehension
+
+# fruits = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango', 'papaya', 'pear', 'pineapple', 'strawberry', 'watermelon', 'blueberry', 'blackberry', 'raspberry', 'coconut', 'cranberry', 'date', 'fig', 'grape', 'guava', 'lemon', 'lime', 'lychee', 'mango', 'nectarine', 'orange', 'papaya', 'peach', 'pear', 'pineapple', 'pomegranate', 'raspberry', 'starfruit', 'strawberry', 'tomato', 'watermelon']
+
+# print(len(fruits))
+
+# for fruit in fruits:
+#     print(fruit)
+
+# newlist = []
+
+# for i in fruits:
+#     if 'w' in i:
+#         newlist.append(i)
+
+# newlist = [x for x in fruits if 'w' in x]    # list comprehension
+# newlist = [x for x in range(len(fruits)) if 'w' in fruits[x]]
+
+# print(newlist)
+
+# numbers = [x for x in range(1, 11) if x % 2 == 0]
+# print(numbers)
+
+# fruits = ["apple", "banana", "cherry", "watermelon", "melon"]
+# newlist = [x if x != "watermelon" else "water" for x in fruits]
+# print(newlist)
+
+# for x in fruits:
+#     if x != "watermelon":
+#         print(x)
+#     else:
+#         print("water")
+
+'''
+Tasks-: 
+Loop List
+
+1. Create a list of numbers from 1 to 20 and loop through it and print the square of each number.
+
+2. Create a list of strings and loop through it and print the string.
+
+Tasks on Loop Comprehension:
+
+1. Create a list of numbers from 1 to 20 and loop through it and print the square of each number.
+
+2. Create a list of vegetables and find the name of the vegetable which starts with “p” & “t” then store it into a new list, print that list.
+
+'''
